a2f388332dfc27f0fe68acff8d3eb1f4eb82c1b0
makadu <makaduapp@outlook.com>
2022-06-26 16:04:04 -0300

    adicionando componente de envio de informações entre componentes

--- src/app/components/parent-component/parent-component.component.ts
+++ src/app/components/parent-component/parent-component.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ParentComponentComponent implements OnInit {
 
   @Input() title!: string
-  @Input() complexData!: {}
+  @Input() complexData!: {email: string, internetProvider: string}
 
   constructor() { }
 
